5468296482e8ae2c543ffae9541e1c456d9b1a0c
Pradeep Sahani <pradeepkumars@smartdatainc.net>
2023-03-28 15:06:53 +0530

    api add work

---
+++ app/route/work.route.js
@@ -0,0 +1,24 @@
+const router = require("express").Router()
+const { validate } = require('../middlewares/validate')
+const cleanerValidator = require('../validator/cleaner.validator')
+const work = require('../controller/work/work.controller')
+const multer = require('multer')
+
+
+// router.post("/", validate(cleanerValidator.add), cleaner.create)
+//     .get("/list", cleaner.cleanerList)
+//     .put("/:cleanerId", validate(cleanerValidator.update), cleaner.update)
+//     .delete("/:cleanerId", cleaner.delete)
+//     .post("/status/:cleanerId", validate(cleanerValidator.updateStatus), cleaner.updateStatus)
+//     .get("/all", cleaner.findAll)
+//     .get("/:cleanerId", cleaner.findById)
+
+    router.post("/add-work",  work.createNewWork)
+
+
+
+
+
+
+
+module.exports=router
